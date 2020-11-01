bba0acf848d0ab596d4c86271e45e046e550506b
Artur Bilyi <whiteartur1997@gmail.com>
2020-11-01 11:54:11 +0200

    completed 3rd task

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { ChangeEvent, KeyboardEvent } from "react";
 import s from "./Greeting.module.css";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string
+    setNameCallback: (event: ChangeEvent<HTMLInputElement>) => void
+    addUser: () => void
+    setError: () => void
+    error: string
+    totalUsers: number
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUser, setError, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error; // need to fix with (?:)
+    const inputClass = error ? s.error : null; // need to fix with (?:)
+
+    const addUserInput = (event: KeyboardEvent<HTMLInputElement>) => {
+      if(event.key === "Enter") {
+        addUser();
+        event.currentTarget.blur();
+      }
+    }
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.greetingWrapper}>
+          <div className={s.inputWrapper}>
+            <input 
+              value={name} 
+              onKeyPress={addUserInput} 
+              onChange={setNameCallback}
+              onFocus={setError} 
+              className={`${s.input} ${inputClass}`}/>
+            <button onClick={addUser} className={s.btn}>add</button>
+          </div>
+            <span className={s.errorMessage}>{error}</span>
+            <span className={s.count}>{totalUsers}</span>
         </div>
     );
 }
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,28 +1,36 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import Greeting from "./Greeting";
+import { UserType } from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> 
+    addUserCallback: (name: string) => void
 }
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
-
-// более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>(""); // need to fix any
-    const [error, setError] = useState<any>(""); // need to fix any
+    const [name, setName] = useState<string>("");
+    const [error, setError] = useState<"" | "You should put a name!">("");
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName(""); // need to fix
+    const setNameCallback = (event: ChangeEvent<HTMLInputElement>) => { 
+        setName(event.currentTarget.value);
     };
+    
     const addUser = () => {
-        alert(`Hello  !`); // need to fix
+      if(name.trim().length > 1) {
+        alert(`Hello, ${name}!`);
+        addUserCallback(name);
+        setName("");
+      } else {
+        setError("You should put a name!")
+      } 
     };
 
-    const totalUsers = 0; // need to fix
+    const setErrorCallback = () => {
+      setError("");
+    }
+
+    const totalUsers = users.length;
 
     return (
         <Greeting
@@ -31,6 +39,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
+            setError={setErrorCallback}
         />
     );
 }
