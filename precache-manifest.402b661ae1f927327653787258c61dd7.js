6c9e16a3ce1dd96a5ea69d0f3114182da11f7866
whiteartur1997 <whiteartur1997@gmail.com>
2020-11-20 09:28:14 +0200

    Updates

---
+++ precache-manifest.402b661ae1f927327653787258c61dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aeb934bac94c78a419f6eaa435e4461",
+    "url": "/Ignat-tasks/index.html"
+  },
+  {
+    "revision": "0a4e78a5afce2763f216",
+    "url": "/Ignat-tasks/static/css/main.22fbafc4.chunk.css"
+  },
+  {
+    "revision": "92faccda99ac8a3f68f1",
+    "url": "/Ignat-tasks/static/js/2.6f3c9da1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Ignat-tasks/static/js/2.6f3c9da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4e78a5afce2763f216",
+    "url": "/Ignat-tasks/static/js/main.7b464d24.chunk.js"
+  },
+  {
+    "revision": "aa7038de03436e2199f5",
+    "url": "/Ignat-tasks/static/js/runtime-main.9c2e51c4.js"
+  }
+]);
